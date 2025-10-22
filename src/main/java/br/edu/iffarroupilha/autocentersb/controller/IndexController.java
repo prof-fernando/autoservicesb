a3f3cf56558c7879525b0f2a5abe1c33602023ac
Fernando <fernando@Fernandos-MacBook-Pro.local>
2025-10-22 20:12:42 -0300

    adicionando novo controlador

---
+++ src/main/java/br/edu/iffarroupilha/autocentersb/controller/IndexController.java
@@ -0,0 +1,15 @@
+package br.edu.iffarroupilha.autocentersb.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class IndexController {
+
+    @GetMapping
+    public String index(){
+        return "Seja bem vindo(a) ao AUTO SERVICE SB";
+    }
+}
